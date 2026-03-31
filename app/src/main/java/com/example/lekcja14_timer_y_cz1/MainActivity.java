ef36afb319fc47a7872a9795b0f7687bee7f248a
Kajerkooo2 <prokajerko79@gmail.com>
2026-03-31 08:03:49 +0200

    layout, podstawowa obsluga czasu i przyciskow start, stop

--- app/src/main/java/com/example/lekcja14_timer_y_cz1/MainActivity.java
+++ app/src/main/java/com/example/lekcja14_timer_y_cz1/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.lekcja14_timer_y_cz1;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +14,15 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textViewTimer;
+    private Button btnStart,btnReset,btnZapisz,btnStop;
+    private ListView listViewZapis;
+
+    private boolean Isrunning = false;
+    private int sekundy = 0;
+    private int minuty = 0;
+    private int godziny = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +34,36 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        textViewTimer = findViewById(R.id.textViewTimer);
+        btnStart = findViewById(R.id.btnStart);
+        btnStop = findViewById(R.id.btnStop);
+        btnReset = findViewById(R.id.btnReset);
+        btnZapisz = findViewById(R.id.btnZapisz);
+
+        Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                if(Isrunning) {
+                    sekundy++;
+                    textViewTimer.setText(String.valueOf(sekundy));
+                }
+                handler.postDelayed(this, 1000);
+            }
+        });
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Isrunning = true;
+            }
+        });
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Isrunning = false;
+            }
+        });
+
     }
+    private String
 }
