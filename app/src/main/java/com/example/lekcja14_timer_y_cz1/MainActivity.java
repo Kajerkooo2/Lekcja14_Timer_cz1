c1cbd85c641daa69df8f289770b20217c68c7128
Kajerkooo2 <prokajerko79@gmail.com>
2026-03-31 08:22:39 +0200

    poprawa adaptera bo wczeniej zly zapis, gotowa aplikacja

--- app/src/main/java/com/example/lekcja14_timer_y_cz1/MainActivity.java
+++ app/src/main/java/com/example/lekcja14_timer_y_cz1/MainActivity.java
@@ -83,7 +83,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 arrayListTime.add(displayTime(sekundy));
-                arrayAdapter.notify();
+                arrayAdapter.notifyDataSetChanged();
             }
         });
     }
